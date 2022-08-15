2038b3da0c6b6e5b527990905bfe8be1e7989250
wsvaio <360117242@qq.com>
2022-08-15 13:40:29 +0800

    重写两个formats

--- utils/formats.ts
+++ utils/formats.ts
@@ -1,66 +1,61 @@
 // 时间格式化
 export function timeFormat(seconds: number, format = "HH:mm:ss") {
-  let time = Math.floor(seconds ?? 0);
 
-  seconds = time;
-  const _seconds = seconds % 60;
+  const o = {
+    "h": 0,
+    "m": 0,
+    "s": 0,
 
-  time = (time - _seconds) / 60;
-
-  const minutes = time;
-  const _minutes = time % 60;
-
-  time = (time - _minutes) / 60;
-  const _hours = time;
-
-  return {
-    _hours, _minutes, _seconds,
-    minutes, seconds,
+    "H": 0,
+    "M": 0,
+    "S": 0,
 
     toString() {
-      return format.replace(/H+/, strNum(_hours, 2))
-        .replace(/m+/, strNum(_minutes, 2))
-        .replace(/s+/, strNum(_seconds, 2));
+      format = format.split("").reverse().join("");
+      for (const k of ["h", "m", "s", "H", "M", "S"]) {
+        const v = String(this[k]).split("").reverse().join("");
+        format = format.replace(new RegExp(`${k}{1,${v.length}}`), v)
+          .replace(new RegExp(`${k}`, "g"), "0");
+      }
+      return format.split("").reverse().join("");
     }
-  };
-
-}
-
-function strNum(num: number, length: number) {
-  let lNum = String(num).split("");
-
-  while (lNum.length < length) {
-    lNum.unshift("0");
   }
-  return lNum.join("");
-}
 
+  let time = o.S = Math.floor(seconds ?? 0);
+  o.s = time % 60;
+
+  time = o.M = (time - o.s) / 60;
+  o.m = time % 60;
+
+  time = o.H = (time - o.m) / 60;
+  o.h = time % 60;
+
+  return o;
+
+}
 
 // 日期格式化
 export function dateFormat(date: string | Date | number | null | undefined, format = "yyyy-MM-dd HH:mm:ss") {
   // sDate = sDate.replace("T", " ").replace("Z", " ").replaceAll("-", "/");
-  if (typeof date == "string") date = date.replace(/-/g, "/");
-  if (!date) date = Date.now();
-  const lFormat = format.split("").reverse();
-  const oDate = new Date(date);
-  const o: any = {
-    "y": oDate.getFullYear(),
-    "M": oDate.getMonth() + 1,
-    "d": oDate.getDate(),
-    "H": oDate.getHours(),
-    "m": oDate.getMinutes(),
-    "s": oDate.getSeconds(),
-    "S": oDate.getMilliseconds(),
-    "w": oDate.getDay()
+  if (typeof date == "string") date = date.replace(/-/g, "/").replace("T", " ").replace("Z", " ");
+  date = new Date(date || Date.now());
+  const o = {
+    "Y": date.getFullYear(),
+    "y": date.getFullYear(),
+    "M": date.getMonth() + 1,
+    "d": date.getDate(),
+    "H": date.getHours(),
+    "m": date.getMinutes(),
+    "s": date.getSeconds(),
+    "S": date.getMilliseconds(),
+    "w": date.getDay()
   };
-  for (let [k, v] of Object.entries(o)) {
-    for (let num of String(v).split("").reverse()) {
-      lFormat[lFormat.indexOf(k)] = num;
-    }
-    for (let [index, value] of Object.entries(lFormat)) {
-      if (k == value) lFormat[Number(index)] = "0";
-    }
+  format = format.split("").reverse().join("");
+  for (let [k, v] of Object.entries<number | string>(o)) {
+    v = String(v).split("").reverse().join("");
+    format = format.replace(new RegExp(`${k}{1,${v.length}}`), v)
+      .replace(new RegExp(`${k}`, "g"), "0");
   }
-  return lFormat.reverse().join("");
+  return format.split("").reverse().join("");
 }
 
