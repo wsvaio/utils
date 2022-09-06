6d63bbed41db420302e6bc99d72ac540ee25d024
wsvaio <360117242@qq.com>
2022-09-06 17:21:06 +0800

    merge fix

--- utils/merge.ts
+++ utils/merge.ts
@@ -4,6 +4,7 @@
  * overwrite: 是否赋值
  * del: 是否删除obj1中obj2不存在的key
  * rtn: 是否返回obj1
+ * has：只有obj1中有的属性才赋值
  */
 
 // 递归可选
@@ -11,19 +12,20 @@ export type DeepPartial<T> = T extends Object
 ? { [P in keyof T]?: DeepPartial<T[P]> }
 : T;
 
-export default function merge<T>(obj1: T, obj2 = <DeepPartial<T> | T>{}, { deep = 1, overwrite = true, del = false, rtn = true } = {}) {
+export default function merge<T extends Object>(obj1: T, obj2 = <DeepPartial<T> | T>{}, { deep = 1, overwrite = true, del = false, rtn = true, has = false } = {}) {
   deep--;
   if (del) {
     const dels = Object.keys(obj1).filter(item => !Object.keys(obj2).includes(item));
     for (const key of dels) delete obj1[key];
   }
   for (const [key, val] of Object.entries(obj2)) {
-    if (!overwrite && ![null, undefined].includes(obj1[key])) continue;
+    if (overwrite && has && [null, undefined].includes(obj1[key])) continue;
     if (typeof val == "object" && deep > 0) {
       (typeof obj1[key] == "object") || (obj1[key] = {});
       merge(obj1[key], val, { deep, overwrite, del, rtn });
     } else {
-      obj1[key] = val;
+      if (!overwrite && ![null, undefined].includes(obj1[key])) continue;
+      obj1[key] = val
     }
   }
   return rtn ? obj1 : "";
