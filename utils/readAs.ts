6d5fa7eda07114dd1b4341aa0645d42e76a30a58
wsvaio <360117242@qq.com>
2022-09-23 16:29:00 +0800

    添加 图片压缩方法

--- utils/readAs.ts
+++ utils/readAs.ts
@@ -1,6 +1,8 @@
-export default (as: "ArrayBuffer" | "BinaryString" | "DataURL" | "Text", blob: Blob, encoding?: string) => new Promise((resolve, reject) => {
-  const reader = new FileReader();
-  reader[`readAs${as}`](blob, encoding);
-  reader.onload = e => resolve(e);
-  reader.onerror = e => reject(e);
-})
+export default <T = string | ArrayBuffer | null>(as: "ArrayBuffer" | "BinaryString" | "DataURL" | "Text", blob: Blob, encoding?: string) =>
+  new Promise<T>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = e => resolve(<T>reader.result);
+    reader.onerror = e => reject(e);
+    reader[`readAs${as}`](blob, encoding);
+  })
+
