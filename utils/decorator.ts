5e3b9e4c1d5581a29f5de8bd036a760796a38dc6
wsvaio <360117242@qq.com>
2022-10-25 11:22:02 +0800

    feat(decorator): 节流防抖添加函数参数

--- utils/decorator.ts
+++ utils/decorator.ts
@@ -2,11 +2,11 @@
  * 防抖
  * 事件函数一段时间后才执行，如果在这段事件内再次调用，则重新计算执行时间
  */
-export function debounce (fn: () => any, time = 1000) {
+ export function debounce (fn: (...args: any[]) => any, time = 1000) {
   let timer: any = null;
-  return () => {
+  return (...args: any[]) => {
     clearTimeout(timer);
-    timer = setTimeout(fn, time);
+    timer = setTimeout(fn, time, ...args);
   }
 
 }
@@ -18,13 +18,11 @@ export function debounce (fn: () => any, time = 1000) {
  * 节流
  * 每隔一段时间，只执行一次
  */
- export function throttle(fn: () => any, time = 1000) {
+ export function throttle(fn: (...args: any[]) => any, time = 1000) {
   let timer: any = null;
-  return () => {
+  return (...args: any[]) => {
     if (timer != null) return;
     timer = setTimeout(() => { timer = null; }, time);
-    fn();
+    fn(...args);
   }
 }
-
-
