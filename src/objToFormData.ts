6b3f901d5ac7bd2b2b0971eddacaff084f002c81
wsvaio <wsvaio@qq.com>
2023-03-10 14:51:37 +0800

    feat: objToFormData

---
+++ src/objToFormData.ts
@@ -0,0 +1,12 @@
+import { is } from "./is"
+
+export const objToFormData = (obj: Record<any, any>) => {
+  const formData = new FormData();
+  for (const [k, v] of Object.entries(obj)) {
+    if (is("Array")(v))
+      v.forEach(item => formData.append(k, item as any));
+
+    else formData.append(k, v);
+  }
+  return formData;
+};
