ecbf771b1e21ba31c8a734b36f535e91409225cc
wsvaio <wsvaio@qq.com>
2023-03-22 13:07:41 +0800

    fix: 未把scripts文件夹添加

--- src/omit.ts
+++ src/omit.ts
@@ -1,7 +1,6 @@
 export const omit = <T extends Object, K extends keyof T>(obj: T, keys: K[], del = false) => {
   const result = <{ [P in Exclude<keyof T, K>]: T[P] }>{};
-  for (const key of Object.keys(obj)) {
-    if (keys.includes(<K>key)) continue;
+  for (const key of Object.keys(obj).filter(key => !keys.includes(key as K))) {
     result[key] = obj[key];
     del && delete obj[key];
   }
--- test/pick.test.ts
+++ test/pick.test.ts
@@ -1,8 +1,7 @@
 import { describe, it } from "vitest";
-import { pick } from "./../src/pick";
 
 describe("pick", () => {
   it("first test", async () => {
-    console.log(pick({ a: 1, b: 2, c: 3 }, ["a", "b"]));
+
   });
 });
