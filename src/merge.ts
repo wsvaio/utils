7dcf4a31ab84819aaf4c8bc1c68b6e6c6bbf2975
wsvaio <wsvaio@qq.com>
2023-03-29 15:48:35 +0800

    perf: 优化merge函数类型，优化deepPartial工具类

--- src/merge.ts
+++ src/merge.ts
@@ -1,31 +1,21 @@
 import { is } from "./is";
 
-// 递归可选
-export type DeepPartial<T> = T extends Function
-  ? T
-  : T extends object
-    ? { [P in keyof T]?: DeepPartial<T[P]> } | Record<any, any>
-    : T;
+export type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
 
 /**
-* 递归合并两个对象到obj1
-* deep: 递归深度
-* overwrite: 是否覆盖obj1已有的属性
-* del: 是否删除obj1中obj2不存在的属性
-* has：只有obj1中有的属性才赋值
-*/
-export const merge = <
-  TObj1 extends object, TObj2 extends DeepPartial<TObj1> = DeepPartial<TObj1>,
->(
-    obj1: TObj1,
-    obj2: TObj2,
-    {
-      deep = 1,
-      overwrite = true,
-      del = false,
-      has = false,
-    } = {},
-  ) => {
+ * 递归合并两个对象到obj1
+ * deep: 递归深度
+ * overwrite: 是否覆盖obj1已有的属性
+ * del: 是否删除obj1中obj2不存在的属性
+ * has：只有obj1中有的属性才赋值
+ */
+export const merge = <TObj1 extends object, TObj2 extends DeepPartial<TObj1> | object>(
+  obj1: TObj1,
+  obj2: TObj2,
+  { deep = 1, overwrite = true, del = false, has = false } = {},
+) => {
   deep--;
   if (del) {
     const dels = Object.keys(obj1).filter(item => !Object.keys(obj2).includes(item));
@@ -44,6 +34,5 @@ export const merge = <
       obj1[key] = val;
     }
   }
-
   return obj1;
 };
