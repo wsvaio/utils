e2a70413084755b5668564e78ca26dcc7b29ed77
wsvaio <wsvaio@qq.com>
2023-10-07 14:25:54 +0800

    fix: 未暴露出两个新加的type

--- src/types.d.ts
+++ src/types.d.ts
@@ -24,13 +24,12 @@ export type ToUnionOfFunction<T> = T extends any ? (x: T) => any : never;
  */
 export type UnionToIntersection<T> = ToUnionOfFunction<T> extends (x: infer P) => any ? P : never;
 
-
 /**
  * 判断两个类型是否相等
  */
-type IsEqual<X, Y> = (<T>() => T extends X ? 1 : 2) extends <T>() => T extends Y ? 1 : 2 ? true : false;
+export type IsEqual<T, U> = (<T1>() => T1 extends T ? 1 : 2) extends <T2>() => T2 extends U ? 1 : 2 ? true : false;
 
 /**
  * 判断属性是否可选
  */
-type IsOptional<O, K extends keyof O> = Partial<Pick<O, K>> extends Pick<O, K> ? true : false;
+export type IsOptional<O, K extends keyof O> = Partial<Pick<O, K>> extends Pick<O, K> ? true : false;
